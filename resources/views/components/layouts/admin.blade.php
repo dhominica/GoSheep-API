79af6ed1ce88fd72602bcc8b9dcf75c4640ca538
Mhdfarhan1 <farhankudap06@gmail.com>
2026-04-30 00:40:51 +0700

    create: icon browser

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Dashboard' }} | GoSheep Admin</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo_app.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/lucide@latest"></script>
